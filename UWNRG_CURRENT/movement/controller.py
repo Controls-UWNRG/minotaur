09d82fd2a623d793aae4b485771835070c83a92a
Sadman Kazi <sadmansazidk@gmail.com>
2016-05-12 20:36:57 -0400

    Fix compile time indentation error

--- UWNRG_CURRENT/movement/controller.py
+++ UWNRG_CURRENT/movement/controller.py
@@ -19,22 +19,22 @@ class Controller():
                           " with a com-port properly.")
 
     def draw_shapes(self, inverted_x_axis, inverted_y_axis, shape_info):
-    for shape in shape_info:
-        shape_type = shape["shape"]
-        shape_count = shape["count"]
-        if shape_type == "circle":
-            if (shape_count > 0):
-                self.move_to_circle_start(inverted_x_axis, inverted_y_axis)
-        for i in range(0, shape_count):
+        for shape in shape_info:
+            shape_type = shape["shape"]
+            shape_count = shape["count"]
             if shape_type == "circle":
-                self.circle_path(inverted_x_axis, inverted_y_axis)
-            elif shape_type == "triangle":
-                self.triangle_path(inverted_x_axis, inverted_y_axis)
-            elif shape_type == "rectangle":
-                self.box_path(inverted_x_axis, inverted_y_axis)
-        if shape_type == "circle":
-            if (shape_count > 0):
-                self.move_to_top_right(inverted_x_axis, inverted_y_axis)
+                if (shape_count > 0):
+                    self.move_to_circle_start(inverted_x_axis, inverted_y_axis)
+            for i in range(0, shape_count):
+                if shape_type == "circle":
+                    self.circle_path(inverted_x_axis, inverted_y_axis)
+                elif shape_type == "triangle":
+                    self.triangle_path(inverted_x_axis, inverted_y_axis)
+                elif shape_type == "rectangle":
+                    self.box_path(inverted_x_axis, inverted_y_axis)
+            if shape_type == "circle":
+                if (shape_count > 0):
+                    self.move_to_top_right(inverted_x_axis, inverted_y_axis)
 
 
     # def box_path(self, inverted_x_axis, inverted_y_axis):
